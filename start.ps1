d278f70cb264a1456270499b582f28f59279fbf9
T-Flet <T-Fletcher@outlook.com>
2021-02-18 18:26:57 +0000

    Split composing of time-consuming frank-api environment from app itself; new start script flag -rebuild is required to recreate the frank-base-envs image

--- start.ps1
+++ start.ps1
@@ -1,1 +1,7 @@
+param ([switch]$rebuild = $false)
+
+if ($rebuild) {
+    docker build -t frank-base-envs -f ..\franky\setup\docker\environments.dockerfile ..\franky\
+}
+
 docker-compose up --build -d
--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if [[ "$1" == "-rebuild" ]]; then
+    docker build -t frank-base-envs -f ../franky/setup/docker/environments.dockerfile ../franky/
+fi
+
 docker-compose up --build -d
